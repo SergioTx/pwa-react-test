6db99ee0f973d6dcc93941282aa4c75764f117f0
Sergio Municio <sergio.municio@soprasteria.com>
2021-01-22 10:07:42 +0100

    feat: improve getTimeFromDates function with a date check. Tests updated

--- src/utils/__tests__/get-time-from-dates.test.ts
+++ src/utils/__tests__/get-time-from-dates.test.ts
@@ -25,4 +25,30 @@ describe('Get time from dates', () => {
       expect(times).toEqual(result);
     }
   );
+
+  // same test data with dates switched
+  it.each([
+    [
+      1611305115551,
+      1611305115551,
+      { days: 0, hours: 0, minutes: 0, seconds: 0 },
+    ],
+    [
+      1611305102854,
+      1611305115551,
+      { days: 0, hours: 0, minutes: 0, seconds: 13 },
+    ],
+    [
+      1611305102854,
+      1611405115551,
+      { days: 1, hours: 3, minutes: 46, seconds: 53 },
+    ],
+  ])(
+    'flips the params if the start date is bigger than the end date',
+    (testFrom, testTo, result) => {
+      const times = getTimeFromDates(testFrom, testTo);
+
+      expect(times).toEqual(result);
+    }
+  );
 });
--- src/utils/get-time-from-dates.ts
+++ src/utils/get-time-from-dates.ts
@@ -9,8 +9,20 @@ const MINUTE_IN_SECONDS = 60;
 const HOUR_IN_SECONDS = MINUTE_IN_SECONDS * 60;
 const DAY_IN_SECONDS = HOUR_IN_SECONDS * 24;
 
+/**
+ * Returns the time difference between two dates split in
+ * days, hours, minutes and seconds
+ *
+ * Flips the params if the starting date is bigger than the ending date.
+ *
+ * @param dateStart - Start date in milliseconds
+ * @param dateEnd - End date in milliseconds
+ * @returns days, hours, minutes and seconds between the two dates
+ */
 const getTimeFromDates = (dateStart: number, dateEnd: number): Times => {
-  let seconds = Math.round((dateStart - dateEnd) / 1000);
+  if (dateStart > dateEnd) return getTimeFromDates(dateEnd, dateStart);
+
+  let seconds = Math.round((dateEnd - dateStart) / 1000);
   const days = Math.trunc(seconds / DAY_IN_SECONDS);
   seconds -= days * DAY_IN_SECONDS;
   const hours = Math.trunc(seconds / HOUR_IN_SECONDS);
